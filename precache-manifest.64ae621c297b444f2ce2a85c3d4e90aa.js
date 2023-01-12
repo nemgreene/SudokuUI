d85d85b488e728477fc7a645fa2288a643d80a19
Nem <vincent.r.greene@gmail.com>
2023-01-12 08:20:47 +0000

    Updates

---
+++ precache-manifest.64ae621c297b444f2ce2a85c3d4e90aa.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5798d977c73604bf8cfcaa78f9399bf5",
+    "url": "/sudokuUI/index.html"
+  },
+  {
+    "revision": "d9021521aff08eb76740",
+    "url": "/sudokuUI/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "5eeae37f03e22124a754",
+    "url": "/sudokuUI/static/css/main.9d49426a.chunk.css"
+  },
+  {
+    "revision": "d9021521aff08eb76740",
+    "url": "/sudokuUI/static/js/2.f0a6ebd2.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/sudokuUI/static/js/2.f0a6ebd2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5eeae37f03e22124a754",
+    "url": "/sudokuUI/static/js/main.0822aa32.chunk.js"
+  },
+  {
+    "revision": "d65b91b1853d550dee89",
+    "url": "/sudokuUI/static/js/runtime-main.5b70ab86.js"
+  }
+]);
